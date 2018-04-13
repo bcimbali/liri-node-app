888b2eb98987ed3dba84f9fd43b5059dad3295f4
Brett Cimbalik <bcimbali@gmail.com>
2018-04-12 21:26:22 -0500

    api keys work and modules connect correctly

--- liri.js
+++ liri.js
@@ -1,4 +1,12 @@
 require("dotenv").config();
 
+var keys = require("./keys.js");
+
+function Spotify(version) {
+    this.version = version;
+}
+
 var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+// var client = new Twitter(keys.twitter);
+
+console.log(spotify);
