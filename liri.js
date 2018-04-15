0293679b6fa395a83ecca5fac3b38f5afdd3d4ec
Brett Cimbalik <bcimbali@gmail.com>
2018-04-15 12:13:24 -0500

    got Twitter API to display data in terminal

--- liri.js
+++ liri.js
@@ -4,9 +4,16 @@ var keys = require("./keys.js");
 
 function Spotify(version) {
     this.version = version;
-}
+};
+
+function Twitter(version) {
+    this.version = version;
+};
 
 var spotify = new Spotify(keys.spotify);
-// var client = new Twitter(keys.twitter);
+var client = new Twitter(keys.twitter);
 
 console.log(spotify);
+console.log('-----------');
+console.log(client);
+console.log('-----------');
