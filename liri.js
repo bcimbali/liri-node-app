4c91dd360e5601ee1617c84dde2d70327d870438
Brett Cimbalik <bcimbali@gmail.com>
2018-04-15 13:05:58 -0500

    twitter api call works as per the npm instructions now

--- liri.js
+++ liri.js
@@ -1,19 +1,33 @@
+
+
+
 require("dotenv").config();
 
 var keys = require("./keys.js");
+
+var Twitter = require('twitter');
+ 
+var client = new Twitter({
+    consumer_key: keys.twitter.consumer_key,
+    consumer_secret: keys.twitter.consumer_secret,
+    access_token_key: keys.twitter.access_token_key,
+    access_token_secret: keys.twitter.access_token_secret
+  });
+ 
+var params = {screen_name: 'boat_test_bot'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
+
 
 function Spotify(version) {
     this.version = version;
 };
 
-function Twitter(version) {
-    this.version = version;
-};
-
 var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+// var client = new Twitter(keys.twitter);
 
-console.log(spotify);
-console.log('-----------');
-console.log(client);
-console.log('-----------');
+// console.log(spotify);
+// console.log('-----------');
